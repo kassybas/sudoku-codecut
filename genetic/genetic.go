fad2eb243b15f15feab0c72b377798013d3d4bd0
Barnabás Kutassy <kutassy.b@gmail.com>
2017-07-24 01:29:05 +0200

    hm

--- genetic/genetic.go
+++ genetic/genetic.go
@@ -26,6 +26,28 @@ func printTopOfGen(children []Child, numOfGen int) {
 	}
 }
 
+func createValues(a [][]byte) []byte {
+	DIM := len(a)
+	values := make([]byte, DIM*DIM)
+	for i := 0; i < DIM*DIM; i++ {
+		values[i] = byte(i%DIM) + 1
+	}
+	for i := 0; i < DIM; i++ {
+		for j := 0; j < DIM; j++ {
+			if a[i][j] == 0 {
+				continue
+			}
+			for k := 0; k < DIM*DIM; k++ {
+				if a[i][j] == values[k] {
+					values = append(values[:k], values[k+1:]...)
+					break
+				}
+			}
+		}
+	}
+	return values
+}
+
 func getHorizontalErrors(line []byte) int {
 	var errors int
 	for i, check := range line {
@@ -51,23 +73,35 @@ func getVerticalErrors(field [][]byte, col int) int {
 	return errors
 }
 
+// func getBoxErrors(field [][]byte) int {
+// 	size := math.sqrt(len(field))
+// 	for i := 0; i < len(field); i += size {
+// 		for j := 0; j < len(field); j += size {
+// 			checkBox(field, i,j)
+// 		}
+// 	}
+// }
+
 func getFitness(candidate [][]byte) int {
 	var errors int
 	for i := 0; i < len(candidate); i++ {
 		errors += getHorizontalErrors(candidate[i])
 		errors += getVerticalErrors(candidate, i)
+
 	}
 	return errors
 }
 
-func generateParent(original [][]byte) [][]byte {
+func generateParent(original [][]byte, values []byte) [][]byte {
 	parent := make([][]byte, len(original))
 
 	for i := 0; i < len(original); i++ {
 		parent[i] = make([]byte, len(original))
 		for j := 0; j < len(original); j++ {
 			if original[i][j] == 0 {
-				parent[i][j] = byte(rand.Intn(len(original)) + 1)
+				valChoice := rand.Intn(len(values))
+				parent[i][j] = values[valChoice]
+				values = append(values[:valChoice], values[valChoice+1:]...)
 			} else {
 				parent[i][j] = original[i][j]
 			}
@@ -82,10 +116,8 @@ func mutateParent(original [][]byte, parentA [][]byte, parentB [][]byte) Child {
 		child[i] = make([]byte, len(original))
 		for j := 0; j < len(original); j++ {
 			if original[i][j] == 0 {
-				parentchoice := rand.Intn(12)
-				if parentchoice >= 10 {
-					child[i][j] = byte(rand.Intn(len(original)) + 1)
-				} else if parentchoice < 5 {
+				parentchoice := rand.Intn(2)
+				if parentchoice == 0 {
 					child[i][j] = parentA[i][j]
 				} else {
 					child[i][j] = parentB[i][j]
@@ -100,8 +132,10 @@ func mutateParent(original [][]byte, parentA [][]byte, parentB [][]byte) Child {
 
 func createFirstGeneration(original [][]byte) []Child {
 	generation := make([]Child, GEN)
+	values := createValues(original)
+
 	for i := 0; i < GEN; i++ {
-		generation[i].genes = generateParent(original)
+		generation[i].genes = generateParent(original, values)
 	}
 	return generation
 }
@@ -135,23 +169,22 @@ func sortByFitness(gen []Child) []Child {
 }
 
 func createNextGeneration(original [][]byte, children []Child) []Child {
-	for i := GEN / 4; i < GEN-(GEN/4); i++ {
-		pa := i - GEN/4
-		pb := GEN - GEN/4 - i
-		children[i] = mutateParent(original, children[pa].genes, children[pb].genes)
-	}
-	for i := GEN - (GEN / 4); i < GEN; i++ {
-		children[i].genes = generateParent(original)
+	values := createValues(original)
+	for i := (GEN / 3) * 2; i < GEN; i++ {
+		children[i].genes = generateParent(original, values)
 		children[i].fitness = GEN
 	}
-
+	for i := GEN / 4; i < GEN; i++ {
+		pa := i % (GEN / 4)
+		pb := rand.Intn(GEN)
+		children[i] = mutateParent(original, children[pa].genes, children[pb].genes)
+	}
 	return children
 }
 
-var GEN int = 10000
+var GEN int = 100000
 
 func Solve(original [][]byte) [][]byte {
-	//	bestFitness := 0
 	rand.Seed(time.Now().UnixNano())
 	generation := createFirstGeneration(original)
 
@@ -159,7 +192,7 @@ func Solve(original [][]byte) [][]byte {
 	printTopOfGen(children, 0)
 
 	var numOfGen int
-	for children[0].fitness != 0 && numOfGen < 10000 {
+	for children[0].fitness != 0 && numOfGen < 100000 {
 		numOfGen++
 		children = sortByFitness(children)
 		children = createNextGeneration(original, children)
